a61ceb0bc5604c940d8120c21f23cb98be5557db
akonshina <arina.konshina@teskalabs.com>
2024-08-11 09:46:32 +0200

    add packages, add timePeriod component, remove unuse files

---
+++ src/components/TopNav.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+// import Contact from './Contact'
+
+const TopNav = () => {
+	return (
+		<div className='topnav'>
+			<span className='topnavtext'>
+				<h1><b>KEYBOARD TRAINER</b></h1>
+			</span>
+		</div>
+	)
+}
+
+export default TopNav
